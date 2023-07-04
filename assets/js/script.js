7c8c956b61eee766f2563c471c3867ae499f2bb2
guilherme melo <guilhermemelodecirqueira@gmail.com>
2023-07-04 11:22:16 -0300

    3 commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,26 +60,7 @@ async function prencher() {
 
           imgLogo.setAttribute("src", i.logo)
 
-          let emprego = document.querySelectorAll(".empregoClone")
-
-          console.log(emprego)
-
-
-          let arrayFront = emprego.filter((v , i) => {
-               let spanSkil = v.querySelectorAll(".lado2 span") 
-               if(spanSkil[i].getAttribute("class") == "Frontend" ){
-                    return true
-               } else{
-                    false
-               }
-              
-          } )
-
-          console.log(arrayFront)
-
-          let arrayback = resp.filter((v) =>  v.role == "Backend")
-
-          let arrayFullstack = resp.filter((v) =>  v.role == "Fullstack")
+          
 
           
 
@@ -101,7 +82,7 @@ async function prencher() {
           let spanLado2 = document.createElement("span")
 
           lado2.append(spanLado2)
-
+          spanLado2.classList.add(i.level)
           spanLado2.innerHTML = i.level
 
           let spanLado3 = document.createElement("span")
@@ -111,31 +92,162 @@ async function prencher() {
           spanLado3.innerHTML = i.role
           
           classEmpregos.append(clone)
+     })
+
+}
+
+function prencherFilter(x){
+     x.forEach( (i)  => {
+          let element = document.querySelector(".emprego")
+
+          let clone = element.cloneNode(true)
+          clone.classList.remove("emprego")
+          clone.classList.add("empregoClone")
+
+          
+
+
+          if(i.new == false && i.featured== false){
+               let classNew = clone.querySelector(".new")
+               let classfeatured = clone.querySelector(".featured")
+               let classBar = clone.querySelector(".bar")
+
+               classNew.style.display = "none"
+               classfeatured.style.display = "none"
+               classBar.style.display = "none"
+          }
+          if( i.featured== false){
+               let classfeatured = clone.querySelector(".featured")
+               let classBar = clone.querySelector(".bar")
+               classfeatured.style.display = "none"
+               classBar.style.display = "none"
+          }
+
+
+          let classEmpregos = document.querySelector(".empregos")
+
+          let contraador = clone.querySelector(".contratante")
+
+          let cargo = clone.querySelector(".contratado")
+
+          let dataPost = clone.querySelector(".datapPoste")
+
+          let periodo = clone.querySelector(".periodo")
+
+          let local = clone.querySelector(".local")
+
+          let imgLogo = clone.querySelector(".imgLogo")
 
 
 
+          
+          contraador.innerHTML = i.company
 
+          cargo.innerHTML = i.position
 
-          let spanSkil = clone.querySelectorAll(".lado2 span")
+          dataPost.innerHTML = i.postedAt
 
-          console.log(spanSkil)
+          periodo.innerHTML = i.contract
 
-          spanSkil.forEach((c) => {
-               c.addEventListener("click" , () => {
-                    if(c.getAttribute("class") == "JavaScript"){
-                             clone.style.display = "flex"
-                    } else{
-                         clone.style.display = "none"
-                    }
-               })
+          local.innerHTML = i.location
+
+          imgLogo.setAttribute("src", i.logo)
+
+          
+
+          
+
+          let lado2 = clone.querySelector(".lado2")
+
+          i.languages.forEach((e) => {
+               let spanLado2 = document.createElement("span")
+               lado2.append(spanLado2)
+               spanLado2.innerHTML = e
+               spanLado2.classList.add(e)
+          })
+          i.tools.forEach((e) => {
+               let spanLado2 = document.createElement("span")
+               lado2.append(spanLado2)
+               spanLado2.innerHTML = e
+               spanLado2.classList.add(e)
           })
 
-          
+          let spanLado2 = document.createElement("span")
 
-          
+          lado2.append(spanLado2)
+          spanLado2.classList.add(i.level)
+          spanLado2.innerHTML = i.level
 
+          let spanLado3 = document.createElement("span")
+
+          lado2.append(spanLado3)
+          spanLado3.classList.add(i.role)
+          spanLado3.innerHTML = i.role
+          
+          classEmpregos.append(clone)
      })
+
 }
+
+
+ async  function clique(){
+     let req = await fetch("./assets/json/data.json")
+     let resp = await req.json()
+     
+     let front = resp.filter((v) => v.role === "Frontend")
+
+     let react = resp.filter((v , i) => {
+          for(let i of v.tools){
+               if(i === "React"){
+                    return true
+               } else{
+                    false
+               }
+          } 
+     })
+
+     console.log(react)
+
+     let spans = document.querySelectorAll(".lado2 span")
+
+     let emprego = document.querySelectorAll(".empregoClone")
+
+     spans.forEach((c) =>{
+          c.addEventListener("click" , () => {
+               if(c.getAttribute("class") == "Frontend"){
+                    emprego.forEach((i) => {
+                         i.remove()
+                    })
+                    prencherFilter(front)
+                    let divFilter = document.querySelector(".filtragem")
+                    divFilter.classList.remove("filtragem")
+                    divFilter.classList.add("filtragemjs")
+               } 
+               if(c.getAttribute("class") == "React"){
+                    emprego.forEach((i) => {
+                         i.remove()
+                    })
+                    prencherFilter(react)
+                    let divFilter = document.querySelector(".filtragem")
+                    divFilter.classList.remove("filtragem")
+                    divFilter.classList.add("filtragemjs")
+               } else{
+                    emprego.forEach((i) => {
+                         i.remove()
+
+                    })
+               }
+          })
+     })
+
+
+     
+}
+
+
+
+
 prencher()
 
+clique()
 
